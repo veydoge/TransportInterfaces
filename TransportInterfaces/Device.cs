087f5e11f652c14f1f29e6edff84f9986aa4ea2f
veydoge <veydoge@gmail.com>
2024-03-21 11:05:20 +0300

    Initial commit

---
+++ TransportInterfaces/Device.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TransportInterfaces
+{
+    public class Device : IDeviceCommands
+    {
+        public int Position { get; set; }
+        public int Rotation { get; set; }
+
+        public void Move(int x)
+        {
+            Position += x;
+        }
+
+        public void Rotate(int x)
+        {
+            Rotation += x;
+        }
+
+
+    }
+
+    public interface IDeviceCommands
+    {
+        public void Move(int x);
+        public void Rotate(int x);
+
+        public int Position { get; set; }
+        public int Rotation { get; set; }
+    }
+}
---
+++ TransportInterfaces/ServercConnection.cs
@@ -0,0 +1,30 @@
+﻿namespace TransportInterfaces
+{
+    public interface ServerConnection
+    {
+
+        public void SendCommand(Command command);
+        public void Close();
+        public void Start(Device device);
+        public void SendData(byte[] data);
+        public void SendString(string data);
+    }
+
+    public class Command
+    {
+        public Command(CommandType Command, int Value)
+        {
+            this.Value = Value;
+            this.Type = Command;
+        }
+        public enum CommandType
+        {
+            Move = 1,
+            Rotate,
+            GetDevice
+        }
+
+        public CommandType Type { get; set; }
+        public int Value { get; set; }
+    }
+}
